0b4eefeac255c7ba3c238e26dea7aed832f8d3eb
codewithkenzo <codewithkenzo@gmail.com>
2025-02-21 18:42:23 +0100

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,26 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
+  optimizeDeps: {
+    include: [
+      '@supabase/auth-ui-react',
+      '@supabase/auth-ui-shared',
+      '@supabase/supabase-js'
+    ]
+  },
   build: {
-    rollupOptions: {
-      external: [
-        '@supabase/auth-ui-react',
-        '@supabase/auth-ui-shared',
-        '@supabase/supabase-js'
-      ]
+    commonjsOptions: {
+      include: [/node_modules/],
+      transformMixedEsModules: true
     }
   }
-}));
+})
